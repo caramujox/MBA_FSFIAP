abfdcebe45117a1b95135661ee61258588bc6dac
Logon Aluno <logonrmlocal@fiap.com.br>
2022-06-25 12:30:20 -0300

    Integration aula2

---
+++ Integration & Develop Tools/HelloApacheCamel/src/main/java/br/com/fiap/caioaraujo/HelloApacheCamel/HelloApacheCamelApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.caioaraujo.HelloApacheCamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApacheCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApacheCamelApplication.class, args);
+	}
+
+}
---
+++ Integration & Develop Tools/HelloApacheCamel/src/test/java/br/com/fiap/caioaraujo/HelloApacheCamel/HelloApacheCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.caioaraujo.HelloApacheCamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApacheCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
