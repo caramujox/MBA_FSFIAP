d6a276cfd34ffac75026edf34bb98d49f23622b6
Logon Aluno <logonrmlocal@fiap.com.br>
2022-06-28 20:33:12 -0300

    fim Camel

--- Integration & Develop Tools/HelloApacheCamel/src/main/java/br/com/fiap/caioaraujo/HelloApacheCamel/HelloApacheCamelApplication.java
+++ Integration & Develop Tools/HelloApacheCamel/src/main/java/br/com/fiap/caioaraujo/HelloApacheCamel/HelloApacheCamelApplication.java
@@ -1,6 +1,10 @@
 package br.com.fiap.caioaraujo.HelloApacheCamel;
 
+import br.com.fiap.caioaraujo.HelloApacheCamel.beans.MyObject;
+import br.com.fiap.caioaraujo.HelloApacheCamel.services.ObjectService;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -12,6 +16,8 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+
+import javax.ws.rs.core.MediaType;
 
 @SpringBootApplication
 public class HelloApacheCamelApplication {
@@ -50,7 +56,27 @@ public class HelloApacheCamelApplication {
                     .dataFormatProperty("prettyPrint", "true");
 
             rest("/api").description("Test Camel Service")
-                    .id("api-route");
+                    .id("api-route")
+                    .post("/test")
+                    .produces(MediaType.APPLICATION_JSON)
+                    .consumes(MediaType.APPLICATION_JSON)
+                    .bindingMode(RestBindingMode.auto)
+                    .type(MyObject.class)
+                    .enableCORS(true)
+                    .to("direct:remoteService")                    ;
+
+            from("direct:remoteService")
+                    .routeId("direct-route")
+                    .log(">>> ${body.id}")
+                    .log(">>> ${body.name}")
+                    .process(new Processor() {
+                        @Override
+                        public void process(Exchange exchange) throws Exception {
+                            MyObject object = (MyObject) exchange.getIn().getBody();
+                            ObjectService.execute(object);
+                            exchange.getIn().setBody(object);
+                        }
+                    });
         }
     }
 
