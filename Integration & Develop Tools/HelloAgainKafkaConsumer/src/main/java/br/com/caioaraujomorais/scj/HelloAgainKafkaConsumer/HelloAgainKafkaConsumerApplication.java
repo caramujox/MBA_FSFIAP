c326a8f5ce0ed44bd4ce9335447e082aadacc0fc
Logon Aluno <logonrmlocal@fiap.com.br>
2022-06-30 21:34:23 -0300

    Hello Again Kafka + Compose com Kafka

---
+++ Integration & Develop Tools/HelloAgainKafkaConsumer/src/main/java/br/com/caioaraujomorais/scj/HelloAgainKafkaConsumer/HelloAgainKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.caioaraujomorais.scj.HelloAgainKafkaConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAgainKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAgainKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ Integration & Develop Tools/HelloAgainKafkaProducer/src/main/java/br/com/caioaraujomorais/scj/HelloAgainKafkaProducer/HelloAgainKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.caioaraujomorais.scj.HelloAgainKafkaProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAgainKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAgainKafkaProducerApplication.class, args);
+	}
+
+}
