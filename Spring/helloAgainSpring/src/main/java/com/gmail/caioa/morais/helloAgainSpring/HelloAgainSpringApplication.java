6569c826145019b9d06664cf0701a656668c46e2
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-09 20:18:50 -0300

    projeto 1 spring

---
+++ Spring/helloAgainSpring/src/main/java/com/gmail/caioa/morais/helloAgainSpring/HelloAgainSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.caioa.morais.helloAgainSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAgainSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAgainSpringApplication.class, args);
+	}
+
+}
---
+++ Spring/helloAgainSpring/src/test/java/com/gmail/caioa/morais/helloAgainSpring/HelloAgainSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.caioa.morais.helloAgainSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAgainSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
