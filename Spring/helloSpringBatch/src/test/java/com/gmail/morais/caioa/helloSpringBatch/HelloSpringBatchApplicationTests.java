0a9c7cc8a8f87d584b681ed60a44a1030a420209
caramujox <caioa.morais@gmail.com>
2022-08-20 16:10:49 -0300

    Spring - Start Spring Batch

---
+++ Spring/helloSpringBatch/src/test/java/com/gmail/morais/caioa/helloSpringBatch/HelloSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.morais.caioa.helloSpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
