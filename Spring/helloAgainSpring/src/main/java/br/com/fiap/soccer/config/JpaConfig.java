17e72e39b8d9e4b12f687e73763f321d02e3929a
caramujox <caioa.morais@gmail.com>
2022-08-20 11:33:22 -0300

    revertendo os erros

--- Spring/helloAgainSpring/src/main/java/br/com/fiap/soccer/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.soccer.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
